a90c7ed627c3f02cb3626efd4be622b273112b3e
trentschnee <tschneweis95@outlook.com>
2020-02-28 22:52:49 -0600

    feat: Add Tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,70 @@
 import React from "react";
 import { handleInitialData } from "../actions/Shared";
 import { connect } from "react-redux";
-import { Grid } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid,Tabs,Tab,Paper,Typography,Box } from "@material-ui/core";
 //maybe make into helpers
 
 import QuestionCard from "./cards/Question";
-class Home extends React.Component {
-  state = {
-    showAnswered: false
-  };
-  render() {
-    const { answeredIds, unansweredIds } = this.props;
-    return (
-      <Grid container spacing={3}>
-        {this.state.showAnswered === true
-          ? answeredIds.map(id => <QuestionCard key={id} id={id} />)
-          : unansweredIds.map(id => <QuestionCard key={id} id={id} />)}
-      </Grid>
-    );
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+const useStyles = theme =>(
+  {
+    paper: {
+      marginBottom: theme.spacing(3),
+    },
   }
+);
+
+
+
+const Home = props => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+    console.log(props)
+    const { answeredIds, unansweredIds,classes } = props;
+
+
+    return (
+      <div><Paper className={classes.paper}>
+      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered indicatorColor="primary">
+      <Tab label="Unanswered Questions"  />
+          <Tab label="Answered Questions" />
+        </Tabs>
+    </Paper>
+    <TabPanel value={value} index={0}>
+    <Grid container spacing={3} >
+        {answeredIds.map(id => <QuestionCard key={id} id={id} />)}
+      </Grid>
+        </TabPanel>
+        <TabPanel value={value} index={1} >
+          
+        <Grid container spacing={3} >
+        {unansweredIds.map(id => <QuestionCard key={id} id={id} />)}
+      </Grid>
+        </TabPanel>
+        
+      
+      </div>
+    )
 }
 
 // Take the state of our store for questions
@@ -39,4 +85,4 @@ function mapStateToProps({ questions, authedUser, users }) {
   };
 }
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(withStyles(useStyles)(Home));
