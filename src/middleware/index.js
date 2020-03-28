1aac698ec4c556515a02eb1675a2b8a58e795687
trentschnee <tschneweis95@outlook.com>
2020-03-28 12:36:06 -0500

    refactor: Code mod on middleware index

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,4 @@
 import thunk from 'redux-thunk';
 import logger from "./Logger"
-import {applyMidleware, applyMiddleware} from "redux";
+import { applyMiddleware} from "redux";
 export default applyMiddleware(thunk,logger)
