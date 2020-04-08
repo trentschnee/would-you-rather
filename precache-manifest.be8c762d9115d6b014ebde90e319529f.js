6d0acefc8f2dd7b6d51563b097b49eaa0f62392d
trentschnee <tschneweis95@outlook.com>
2020-04-08 14:27:55 -0500

    Updates

---
+++ precache-manifest.be8c762d9115d6b014ebde90e319529f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de3db8d6e9492f70d944fc753b79b84",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "451aab83af4a2504a2da",
+    "url": "/would-you-rather/static/css/main.69e448f6.chunk.css"
+  },
+  {
+    "revision": "ff618067240b1808e167",
+    "url": "/would-you-rather/static/js/2.34f39b99.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/would-you-rather/static/js/2.34f39b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451aab83af4a2504a2da",
+    "url": "/would-you-rather/static/js/main.504df52c.chunk.js"
+  },
+  {
+    "revision": "9cb96579a1bbbf7bef47",
+    "url": "/would-you-rather/static/js/runtime-main.48ac087f.js"
+  }
+]);
