4cee426a004b69a8def421aec411acb5d724a588
trentschnee <tschneweis95@outlook.com>
2020-04-08 14:56:24 -0500

    chore: Deploy site

--- src/components/404Page.js
+++ src/components/404Page.js
@@ -1,6 +1,6 @@
 
 const FourOFour = props =>{
-return("Error code 404: This page couldn't be found."
+return("Error code 404: This page couldn't be found. If you haven't logged in yet, please click 'LOGIN' on the top right corner of this page."
 )
 }
 export default FourOFour;
